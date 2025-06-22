dcf9bc7eabb9165be0114d3d0c2f3f44e184f837
Guillaume Perrier <guillaumeperrier@Ordinatuillaume.lan>
2025-06-22 23:15:36 +0200

    Premier commit HEYDTRACK

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: 20 }}>
+      <h1>Bienvenue sur HEYDTRACK</h1>
+      <p>App de coaching sportif — en cours de développement.</p>
+    </div>
+  );
+}
